97f9359c560b67cf46e239707273891104c768b2
AndreaXyna <andreaswihartdecoster@gmail.com>
2024-01-23 15:05:28 -0600

    Player Controller merge

--- Assets/Scripts/Player/Slicer.cs
+++ Assets/Scripts/Player/Slicer.cs
@@ -28,12 +28,12 @@ public class Slicer : MonoBehaviour
         var a = mesh.GetSubMesh(0);
         Sliceable sliceable = objectToCut.GetComponent<Sliceable>();
 
-        if (sliceable == null)
+        /*if (sliceable == null)
         {
             throw new NotSupportedException("Cannot slice non sliceable " +
                 "object, add the sliceable script to the object or inherit " +
                 "from sliceable to support slicing");
-        }
+        }*/
 
         //Create left and right slice of hollow object
         SlicesMetadata slicesMeta = 
