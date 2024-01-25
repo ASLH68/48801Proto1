2325142b0c3ecc5093601872caca1e9ed0681aee
AndreaXyna <andreaswihartdecoster@gmail.com>
2024-01-25 13:00:58 -0600

    Documentation

--- Assets/Scripts/Player/Sliceable/SlicedActions/ExOnSlice.cs
+++ Assets/Scripts/Player/Sliceable/SlicedActions/ExOnSlice.cs
@@ -1,3 +1,11 @@
+/*****************************************************************************
+// File Name :         ExOnSlice.cs
+// Author :            Andrea Swihart-DeCoster
+// Creation Date :     01/25/24
+//
+// Brief Description : Example for how OnSliced is used
+*****************************************************************************/
+
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -9,6 +17,8 @@ public class ExOnSlice : OnSliced
     {
         // Calls the parent sliced action
         base.SlicedAction();
+
+        // object specific functionality
         Debug.Log("derived sliced");
     }
 }
