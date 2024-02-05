08ae652417a8c6a4ffd8e0d1cfb74cd5b6af656d
MagicTurtle013 <kotovskykyle@gmail.com>
2024-02-04 22:31:08 -0600

    tried something and it won't work :(

--- Assets/Scripts/Player/Sliceable/SlicedActions/PushObject.cs
+++ Assets/Scripts/Player/Sliceable/SlicedActions/PushObject.cs
@@ -8,7 +8,7 @@ public class PushObject : OnSliced
     [SerializeField] float _xImpulse = 0f;
     [SerializeField] float _yImpulse = 0f;
     [SerializeField] float _zImpulse = 0f;
-    [SerializeField] float _timeRemaining = 0f;
+
     public override void SlicedAction()
     {
         // Calls the parent sliced action
@@ -17,27 +17,6 @@ public class PushObject : OnSliced
         // object specific functionality
         Debug.Log("derived sliced");
 
-        if (_timeRemaining > 0)
-        {
-            _timeRemaining -= Time.deltaTime;
-        }
-        else
-        {
-            _timeRemaining = 0f;
-            _object.GetComponent<Rigidbody>().AddForce(new Vector3(_xImpulse, _yImpulse, _zImpulse), ForceMode.Impulse);
-        }
+        _object.GetComponent<Rigidbody>().AddForce(new Vector3(_xImpulse, _yImpulse, _zImpulse), ForceMode.Impulse);
     }
-
-    /*private void Update()
-    {
-        if (_timeRemaining > 0)
-        {
-            _timeRemaining -= Time.deltaTime;
-        }
-        else
-        {
-            _timeRemaining = 0f;
-            _object.GetComponent<Rigidbody>().AddForce(new Vector3(_xImpulse, _yImpulse, _zImpulse), ForceMode.Impulse);
-        }
-    }*/
 }
