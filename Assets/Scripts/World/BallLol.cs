d5ef85ef801f7350cda6feed34dff355214d4444
BreadReaver <ocreighton@mail.bradley.edu>
2024-02-06 22:17:36 -0600

    admitting defeat

--- Assets/Scripts/World/BallLol.cs
+++ Assets/Scripts/World/BallLol.cs
@@ -1,11 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BallLol : MonoBehaviour
 {
-   
-    void Update() {
-      transform.Translate(200f * Time.deltaTime, 0, 0);
+    public Rigidbody rb;
+    public float forceAmount = 10;
+
+    private void Start()
+    {
+        //rb.AddForce(Vector3.forward * forceAmount, ForceMode.Impulse);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(Vector3.forward * forceAmount);
     }
 }
+
+   
